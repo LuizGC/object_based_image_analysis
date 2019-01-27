b1ea22501acd3c72ca32cdd47ac67b5811b90109
Luiz Costa <luiz.guimaraes@produtoragro.com.br>
2019-01-27 15:53:55 -0200

    change quickshift to fefelzenszwalb

--- code/create_segmentation.py
+++ code/create_segmentation.py
@@ -1,7 +1,7 @@
 import os
 from osgeo import gdal
 import numpy as np
-from skimage.segmentation import quickshift 
+from skimage.segmentation import felzenszwalb 
 
 gdal.AllRegister()
 
@@ -31,18 +31,32 @@ segments_path = os.path.join(TILES_PATH, 'segments.tif')
 if os.path.exists(segments_path):
 	os.remove(segments_path)
 bands_data = []
+n_bands = 0
 for file in os.listdir(TILES_PATH):
 	if file.endswith(".tif"):
 		path = os.path.join(TILES_PATH, file)
 		raster_dataset = gdal.Open(path, gdal.GA_ReadOnly)
 		setup_dict_raster(raster_dataset)
 		for b in get_array_data(raster_dataset):
+			n_bands = n_bands+1
 			bands_data.append(b) 
 
 bands_data = np.dstack(b for b in bands_data)
 
-segments = quickshift(bands_data, kernel_size=7, max_dist=3, ratio=0.35, convert2lab=False)
-print('Número de segmentos: ', len(np.unique(segments)))
+band_segmentation = []
+for i in range(n_bands):
+    band_segmentation.append(felzenszwalb(bands_data[:, :, i], scale=15, sigma=0.25, min_size=4))
+
+const = [b.max() + 1 for b in band_segmentation]
+segmentation = band_segmentation[0]
+for i, s in enumerate(band_segmentation[1:]):
+    segmentation += s * np.prod(const[:i+1])
+_, labels = np.unique(segmentation, return_inverse=True)
+segments_felz = labels.reshape(bands_data.shape[:2])
+	
+segments = segments_felz
+segment_ids = np.unique(segments)
+print("Felzenszwalb segmentation. %i segments." % len(segment_ids))
 
 outDs = dict_raster['driver'].Create(segments_path, dict_raster['cols'], dict_raster['rows'], 1, gdal.GDT_Int32)
 
